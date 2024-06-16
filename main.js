0314d5868f8b3fd82d78829851e07763f48bcdcf
Nasser Mamoune <medsaleckmamoune@gmail.com>
2024-06-16 14:07:24 +0000

    Added Website

---
+++ main.js
@@ -0,0 +1,10 @@
+// Start Menu Function
+function showSidebar() {
+    const sidebar = document.querySelector('.side_bar');
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar() {
+    const sidebar = document.querySelector('.side_bar');
+    sidebar.style.display = 'none'
+  }
+// End Menu Function
